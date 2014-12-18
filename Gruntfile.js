ab46b4e814d1ac71f8c55ad2ed4f14548ae94a05
Constantine Karnaukhov <genteelknight@gmail.com>
2014-12-19 00:22:29 +0400

    Alpha

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,64 +1,94 @@
 module.exports = function (grunt) {
 	'use strict';
-	
+
+	// Grunt's tasks options
 	grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json'),
-		
+		weking: grunt.file.readJSON('package.json'),
+		theme: grunt.file.readJSON('package.json'),
+
+		// Compile Less source
 		less: {
-			dev: {
+			main: {
 				options: {
 					expand: true,
 					banner: '/*!\n' +
-					' * Name: <%= pkg.name %>\n' +
-					' * Description: <%= pkg.description %>\n' +
-					' * Version: <%= pkg.version %>\n' +
-					' * URI: <%= pkg.url %>\n' +
-					' * Author: <%= pkg.author %>\n' +
-					' * License: <%= pkg.license %>\n' +
+					' * Name: <%= weking.name %>\n' +
+					' * Description: <%= weking.description %>\n' +
+					' * Version: <%= weking.version %>\n' +
+					' * URI: <%= weking.url %>\n' +
+					' * Author: <%= weking.author %>\n' +
+					' * License: <%= weking.license %>\n' +
 					' */\n'
 				},
 				files: {
-					"dist/css/weking.css": "src/less/weking.less",
+					"dist/css/weking.css": "src/less/weking.less"
+				}
+			},
+			theme: {
+				options: {
+					expand: true,
+					banner: '/*!\n' +
+					' * Name: <%= theme.name %>\n' +
+					' * Description: <%= theme.description %>\n' +
+					' * Version: <%= theme.version %>\n' +
+					' * URI: <%= theme.url %>\n' +
+					' * Author: <%= theme.author %>\n' +
+					' * License: <%= theme.license %>\n' +
+					' */\n'
+				},
+				files: {
 					"dist/css/weking-theme.css": "src/less/weking-theme.less"
 				}
 			}
 		},
-		
-		cssmin: {
-			minify: {
-				expand: true,
-				cwd: 'dist/css/',
-				src: 'weking.css',
-				dest: 'dist/css/',
-				ext: '.min.css'
-			}
-		},
-		
+
+		// Beautify compiled files
 		cssbeautifier: {
-			files : ["dist/css/weking.css"],
+			main: ["dist/css/weking.css"],
+			theme: ["dist/css/weking-theme.css"],
 			options : {
 				indent: '	',
 				openbrace: 'end-of-line',
 				autosemicolon: false
 			}
 		},
-		
+
+		//  Minimize compiled files
+		cssmin: {
+			main: {
+				expand: true,
+				cwd: 'dist/css/',
+				src: 'weking.css',
+				dest: 'dist/css/',
+				ext: '.min.css'
+			},
+			theme: {
+				expand: true,
+				cwd: 'dist/css/',
+				src: 'weking-theme.css',
+				dest: 'dist/css/',
+				ext: '.min.css'
+			}
+		},
+
+		// Watch - to compile files when sources will be changed
 		watch: {
-			scripts: {
-				files: ['src/less/*.less'],
-				tasks: ['compile']
+			src: {
+				files: ['src/less/**/*.less'],
+				tasks: ['newer:less', 'cssbeautifier', 'cssmin', 'watch']
 			}
 		}
 
 	});
-	
+
+	// Load Grunt modules
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-cssbeautifier');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-newer');
-	
-	grunt.registerTask('compile', ['newer:less', 'cssbeautifier', 'cssmin', 'watch']);
-	grunt.registerTask('default', ['less', 'cssbeautifier', 'cssmin']);
+
+	// Default task: compile weking and theme, format and minify css and watch for next manipulations
+	grunt.registerTask('default', ['less', 'cssbeautifier', 'cssmin'/*, 'watch'*/]);
 };
